81e5e26721bac67cef5e7119d137287767564501
MarrotteK <kyle.marrotte@gmail.com>
2015-10-21 17:20:46 -0400

    Added note about compatibility
    #NOTE:  Must add y1[1] = NULL for current format of other token.
    functions

--- tokenoverwrite.R
+++ tokenoverwrite.R
@@ -1,5 +1,6 @@
 token.overwrite <- function(x){
   x = "Calculate <V1,1,10> + <V2,5,20>"
+  #NOTE:  Must add y1[1] = NULL for current format of other token. functions
   for (i in 1:length(y1)){
     x = sub("<.*?>", y1[i], x)}
   
