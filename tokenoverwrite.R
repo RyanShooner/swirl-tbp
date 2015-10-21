e03b72a27cdf6387568794a9fc2e11b1fba2a09f
MarrotteK <kyle.marrotte@gmail.com>
2015-10-21 17:45:43 -0400

    Bugs + Testing
    Fixed a small bug, and added extra comments to be turned on or off for
    testing.

--- tokenoverwrite.R
+++ tokenoverwrite.R
@@ -1,8 +1,11 @@
-token.overwrite <- function(x){
-  x = "Calculate <V1,1,10> + <V2,5,20>"
-  #NOTE:  Must add y1[1] = NULL for current format of other token. functions
-  for (i in 1:length(y1)){
-    x = sub("<.*?>", y1[i], x)}
+token.overwrite <- function(x, y){
+  #For testing, uncomment the following two lines, or comment them for usage with other functions:
+  #x = "Calculate <V1,1,10> + <V2,5,20>"
+  #y = list(X = NULL, V1 = 5, V2 = 3)
+  #NOTE:  Must add y[1] = NULL for current format of other token. functions.  Will be changed later.
+  y[1] = NULL
+  for (i in 1:length(y)){
+    x = sub("<.*?>", y[i], x)}
   
   
   return(x)}
