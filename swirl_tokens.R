ae748f0d1fb42e92c5d1717b232663645d4af651
Kyle A. Marrotte <kyle.marrotte@gmail.com>
2015-11-03 13:20:58 -0500

    Cleared workspace, improved swirl_tokens
    Moved old functions to 'Depreciated' folder, and made some functionality
    changes to swirl_tokens.R

--- swirl_tokens.R
+++ swirl_tokens.R
@@ -11,18 +11,17 @@
 token.replace <- function(y,x){
   
   for (i in 1:length(y)){
-    x = sub("<.*?>", y[[i]][2], x)} #Not ideal.  Need to change y[[i]][2] to account for all possible codes, maybe?
+    x = sub(paste0("<",y[[i]][1],">"), y[[i]][2], x)} #Not ideal.  Need to change y[[i]][2] to account for all possible codes, maybe?
   return(x)
 }
 
-create.tokens <- function(token.str) { #token.str will be code to input, output.to will be the value to apply token.replace to
-  eval(parse(text = token.str))   #pulls the code out of the character string and executes it using magic
-  a = ls.str(pattern = "V")       # creates a vector of tokens in the function environment
+create.tokens <- function(.token.str) { #token.str will be code to input, output.to will be the value to apply token.replace to
+  eval(parse(text = .token.str))   #pulls the code out of the character string and executes it using magic
+  a = ls.str()       # creates a vector of tokens in the function environment
   x = list()                      #creates blank list
   for (i in 1:length(a)){
-    x[i] = list(c(a[i], get(a[i]))) #This took forever to figure out.  Had to add a list into the list?
+    x[[i]] = c(a[i], get(a[i])) #This took forever to figure out.  Had to add a list into the list?
   }
-  #z = token.replace(x, output.to) #takes the output of create.tokens and applys it to the desired output value
   return(x)
 }
 
@@ -31,9 +30,9 @@ create.tokens <- function(token.str) { #token.str will be code to input, output.
 # Test case, use create.tokens and token.replace to
 # update the output and answer strings
 ##########################################################
-#tokens = "V1 = sample(1:10,1); V2 = V1 + sample(3:20,1)"
-#output = "create a vector x that contains <V1> through <V2>"
-#answer = "x = <V1>:<V2>"
+tokens = "V1 = sample(1:10,1); V2 = V1 + sample(3:20,1)"
+output = "create a vector x that contains <V1> through <V2>"
+answer = "x = <V2>:<V1>"
 
 
 
